157cc995cec4637794aa8edeebb05d8bc5326ccd
Богданыч <xachwork2@gmail.com>
2022-04-05 19:52:55 +0300

    Initial commit

---
+++ Assets/Codebase/Services/Input/IInputService.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Codebase.Services.InputLogic 
+{
+    interface IInputService
+    {
+        public Vector2 Axis { get; }
+    }
+}
---
+++ Assets/Codebase/Services/Input/PCMovementInput.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Codebase.Services.InputLogic
+{
+    public class PCMovementInput : MovementInput
+    {
+        public override Vector2 Axis => GetKeyboardAxis();
+
+        private Vector2 GetKeyboardAxis()
+        {
+            return new Vector2(Input.GetAxisRaw(HorizontalAxis), Input.GetAxisRaw(VerticalAxis));
+        }
+    }
+}
